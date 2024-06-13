79ef8977d740603281a2e13930b4dab4e0385a2d
tmash <132013534+7m45h@users.noreply.github.com>
2024-06-13 10:53:44 +0530

    clean

--- src/inc/apple.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_rect.h>
-
-void       APPLE_init(void);
-void       APPLE_set_random_position(void);
-SDL_FRect* APPLE_get_position(void);
-void       APPLE_render(void);
--- src/inc/scoreboard.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int  SCOREB_init(void);
-void SCOREB_update(int score);
-void SCOREB_render(void);
-void SCOREB_deinit(void);
