517b89115f7ceb29e7f9f2897affcd3f92c1c3bf
tmash <132013534+7m45h@users.noreply.github.com>
2024-07-14 12:16:06 +0530

    save world state json

--- src/sl_manager/sl_manager.c
+++ src/sl_manager/sl_manager.c
@@ -8,6 +8,8 @@
 #include "sl_manager.h"
 #include "struct2json/struct2json.h"
 
+#define SAFE_FILE_PATH "./nake_save.json"
+
 int SLM_save(World* world)
 {
   struct json_object* world_state = STJ_world(world);
@@ -17,7 +19,13 @@ int SLM_save(World* world)
     return 1;
   }
 
-  LOGG(json_object_to_json_string_ext(world_state, JSON_C_TO_STRING_PRETTY));
+  const char* world_state_serialized = json_object_to_json_string(world_state);
+
+  FILE* save_file = fopen(SAFE_FILE_PATH, "w");
+  if (save_file != NULL) fputs(world_state_serialized, save_file);
+  else LOGG("fopen failed");
+
+  fclose(save_file);
   json_object_put(world_state);
 
   return 0;
