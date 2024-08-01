0947d5b574a1a982fad977d2657da44d93c10f2f
tmash <132013534+7m45h@users.noreply.github.com>
2024-08-01 11:31:56 +0530

    argp

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <argp.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -12,7 +13,48 @@
 #define DEFAULT_GRID_CELL_COUNT_Y 32
 #define DEFAULT_UPDATE_INTERVAL   10
 
-int main(void)
+const char* argp_program_version    = "3.0.1";
+static char doc[]                   = "simple snake game made with SDL2 and written in C";
+static struct argp_option options[] = {
+  { "cell_size",       's',       "CELL_SIZE",  OPTION_ARG_OPTIONAL,             "grid cell size", 1 },
+  { "cell_count_x",    'x',    "CELL_COUNT_X",  OPTION_ARG_OPTIONAL, "grid cell count x / column", 1 },
+  { "cell_count_y",    'y',    "CELL_COUNT_Y",  OPTION_ARG_OPTIONAL,    "grid cell count y / row", 1 },
+  { "update_interval", 'i', "UPDATE_INTERVAL",  OPTION_ARG_OPTIONAL,       "how fast the nake is", 2 },
+  { 0 }
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+  STTiconf* conf = state->input;
+
+  switch (key)
+  {
+    case 's':
+    conf->grid_cell_size    = arg ? atoi(arg) : DEFAULT_GRID_CELL_SIZE;
+    break;
+
+    case 'x':
+    conf->grid_cell_count_x = arg ? atoi(arg) : DEFAULT_GRID_CELL_COUNT_X;
+    break;
+
+    case 'y':
+    conf->grid_cell_count_y = arg ? atoi(arg) : DEFAULT_GRID_CELL_COUNT_Y;
+    break;
+
+    case 'i':
+    conf->update_interval   = arg ? atoi(arg) : DEFAULT_UPDATE_INTERVAL;
+    break;
+
+    default:
+    return ARGP_ERR_UNKNOWN;
+  }
+
+  return 0;
+}
+
+static struct argp parser = { options, parse_opt, NULL, doc, NULL, NULL, NULL };
+
+int main(int argc, char** argv)
 {
   Game* game = NULL;
 
@@ -23,6 +65,8 @@ int main(void)
       DEFAULT_GRID_CELL_COUNT_Y,
       DEFAULT_UPDATE_INTERVAL
     };
+
+    argp_parse(&parser, argc, argv, 0, NULL, &conf);
 
     game = GAME_create(WINDOW_TITLE, &conf);
   }
